c7870bf7f76b261c81180033d9811b9769702458
Saqlain Afroz <sqn3680@gmail.com>
2026-04-03 16:50:11 +0530

    Added image file

--- play.py
+++ play.py
@@ -1,11 +1,12 @@
 from interferometer  import Interferometer
 import matplotlib.pyplot as plt
 
-detector = Interferometer(amplitude=0.01, sampling_rate=512)
+detector = Interferometer(amplitude=0.01, sampling_rate=512, duration=12)
 
 signal, idx = detector.setup_interferometer(inject_gw=True)
 
 plt.plot(detector.time, signal)
 plt.axvline(detector.time[idx], color='r', linestyle='--', label="GW start")
 plt.legend()
+plt.savefig('Signal.png')
 plt.show()
