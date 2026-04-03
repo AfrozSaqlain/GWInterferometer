24ecdad0c8a1d325e9d7c28d11964617b331dfc0
Saqlain Afroz <sqn3680@gmail.com>
2026-04-03 18:42:57 +0530

    Updated Image

--- play.py
+++ play.py
@@ -5,6 +5,7 @@ detector = Interferometer(amplitude=1, sampling_rate=512, duration=12, target_sn
 
 signal, idx = detector.setup_interferometer(inject_gw=True)
 
+plt.figure(figsize=(20, 6))
 plt.plot(detector.time, signal)
 plt.savefig('Signal.png')
 plt.show()
